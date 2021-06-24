824c217433dccb5e0dadecf9d1968b7a647fda48
Микола Матіос <mikolka09@gmail.com>
2021-06-24 17:52:58 +0300

    24.06.2021

---
+++ WA_2019_Net/Controllers/PageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WA_2019_Net.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult show(int id)
+        {
+            ViewBag.Id = id;
+            ViewBag.Title = id;
+            //TODO:Обратимся к базе данных за записью 10
+            return View();
+        }
+
+    }
+   
+}
